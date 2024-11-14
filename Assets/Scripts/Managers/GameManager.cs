cb3f7a1dac2c20f5b979d1934066b358d444bf96
gwanyoungkim719 <0000kgy@naver.com>
2024-11-14 20:28:14 +0900

    [Add] UI

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,8 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,8 +11,14 @@ public class GameManager : MonoBehaviour
     public Transform Player { get; private set; }
     public ObjectPool ObjectPool { get; private set; }
     public ParticleSystem EffectParicle;
-
+    
     [SerializeField] private string playerTag = "Player";
+
+    private HealthSystem playerHealthSystem;
+
+    [SerializeField] private TextMeshProUGUI waveTxt;
+    [SerializeField] private Slider hpGaugeSlider;
+    [SerializeField] private GameObject gameoverUI;
 
     private void Awake()
     {
@@ -17,5 +27,31 @@ public class GameManager : MonoBehaviour
 
         ObjectPool = GetComponent<ObjectPool>();
         EffectParicle = GameObject.FindGameObjectWithTag("Particle").GetComponent<ParticleSystem>(); //일단 한번쓰고 마니까 Find 함수로 가져온 것
+
+        playerHealthSystem = Player.GetComponent<HealthSystem>();
+        playerHealthSystem.OnDamage += UpdateHealthUI;
+        playerHealthSystem.OnHeal += UpdateHealthUI;
+        playerHealthSystem.OnDeath += GameOver;
+    }
+
+    private void GameOver()
+    {
+        //UI 켜줌
+        gameoverUI.SetActive(true);
+    }
+
+    private void UpdateHealthUI()
+    {
+        hpGaugeSlider.value = playerHealthSystem.CurrentHealth / playerHealthSystem.MaxHealth;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //현재 씬을 다시불러옴
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
